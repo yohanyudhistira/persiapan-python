a8e13e31f5e6b6c7872aee515c2a26436095dc1e
Yohan <yohan.yudhistira90@gmail.com>
2021-02-26 17:16:16 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+
+print('Hello World!')
+try:
+    r = requests.get('https://google.com')
+    print(r.status_code)
+    if r.status_code == 200:
+        print(r.text)
+
+except Exception as e:
+    print('404 NOT FOUND', e)
+
+
